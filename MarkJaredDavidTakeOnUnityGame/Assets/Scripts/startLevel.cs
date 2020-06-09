e4b383a9dd82ff25c8e77a8ed05283a775aa7980
David Kriz <davkri22@bergen.org>
2020-06-09 11:35:34 -0400

    Added title screen and start button

---
+++ MarkJaredDavidTakeOnUnityGame/Assets/Scripts/startLevel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startLevel : MonoBehaviour
+{
+    public void changeScreen(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
